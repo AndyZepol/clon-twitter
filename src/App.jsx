c90240cc4e7c2790d208efffd997d10b0b72e5ea
Andrea López Flores <andyzepol03@gmail.com>
2024-10-09 21:49:25 -0600

    Modificacion del app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from "react";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [posts, setPosts] = useState([]);
+  const [post, setPost] = useState("");
+
+  useEffect(() => {
+    console.log("Componenete cargado");
+  }, [post,posts]);
+
+  const handleSubmitPost = (event) => {
+    event.preventDefault();
+    if(post){
+      // subir ese post al arreglo
+      setPosts([...posts, post]);
+      setPost("");
+      console.log([...posts, post]);
+    }
+  };
+
+  const handleInputChange = (event) => {
+    setPost(event.target.value);
+  };
+
+  const handleDeletePost = (index) => {
+    const newPost =posts.filter((item, i) => i !== index);
+    setPosts(newPost);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Clon twitter</h1>
+      <form onSubmit={handleSubmitPost}>
+        <input
+          type="text"
+          placeholder="Escribe algo"
+          value={post}
+          onChange={handleInputChange}
+        />
+        <input type="submit" value="Postear" />
+      </form>
+      <p>Actual post: {post}</p>
+      <section>
+      {posts.map((item,index)=> (
+         <div key={index}>
+         <p>{item}</p>
+         <button onClick={() => handleDeletePost(index)}>Borrar</button>
+         </div>
+      ))}
+      </section>
+     
     </>
-  )
+  );
 }
 
-export default App
+export default App;
